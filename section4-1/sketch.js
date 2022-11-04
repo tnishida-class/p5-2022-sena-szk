34fefbe54816a09d2a8d472eeaa949f3998070ad
sena-szk <2110841h@gsuite.kobe-u.ac.jp>
2022-11-04 15:57:58 +0900

    changed.sec4-2

--- section4-1/sketch.js
+++ section4-1/sketch.js
@@ -19,17 +19,33 @@ function setup(){
 
   // ここから平均・最大・最小を求めます
   let average, largest, smallest;
-  // BLANK[1]　平均値（ヒント average = 合計 / 配列の長さ）
-
+   avarage=sum/scores.length;
+   console.log(avarage);
   largest = 0;
   for(let i = 0; i < scores.length; i++){
+    if(largest<scores[i]){
+      largest=scores[i];
+    }
+    else{
+      largest=largest;
+    }
+   
     // BLANK[2]　ヒント：今までの最大値 largest と scores[i] を比較する
+
   }
+  console.log(largest);
 
   smallest = 100;
   for(let i = 0; i < scores.length; i++){
+    if(smallest>scores[i]){
+      smallest=scores[i];
+    }
+    else{
+      smallest=smallest;
+    }
     // BLANK[3]　ヒント：最小値とだいたい同じ
   }
+  console.log(smallest);
 
   // ここから棒グラフを描いていきます。まずは背景に横線をn本引く
   const n = 10;
@@ -40,11 +56,19 @@ function setup(){
   for(let i = 0; i < scores.length; i++){
     const dx = width / scores.length;
     const h = height * scores[i] / 100;
+    if(scores[i]==largest){
+      fill(255,0,0,);
+    }
+    else if(scores[i]==smallest){
+      fill(0,0,255);
+    }
+    
     // BLANK[4] ヒント: 条件分岐を使って色を変更します
     rect(i * dx + 2, height - h, dx - 4, h);
-    fill(0);
+    fill(128);
     text(scores[i].toPrecision(3), i * dx, height - h);
   }
-
+stroke(0,255,0);
+line(0,height-avarage*(height/100),400,height-avarage*(height/100));
   // BLANK[5] 平均点の線を引きます
 }
